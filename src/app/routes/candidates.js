20299cb1e11adcaa52768c0b0a1e3bb650dfbdcf
Alvaro Separovich <alvasepro@gmail.com>
2019-11-02 20:46:10 -0300

    Candidate API completed

--- src/app/routes/candidates.js
+++ src/app/routes/candidates.js
@@ -4,13 +4,17 @@ module.exports = (app,Router)=>{
 
     const router = Router();
 
-    router.get('/', async (req, resp)=>{
+    router.get( '/', async (req, resp)=>{
+    
         allCandidates = await Candidates.find();
         return resp.send( allCandidates );
+    
     })
 
-    router.post('/register', async (req,resp)=>{
+    router.post( '/register', async (req,resp)=>{
+
         const {email} = req.body;
+        
         try{
             if( await Candidates.findOne({email}) ){
                 return resp.status(400).send({erro:'This email has already been used'})
@@ -26,19 +30,54 @@ module.exports = (app,Router)=>{
         catch(err){
 
             console.log(err);
-            return resp.status(400).send({error:'Fail to register candidate'});
+            return resp.status(400).send({error:'The operation failed'});
 
         }
+
     });
 
-    router.put('/', async (req, resp)=>{
-        allCandidates = await Candidates.find();
-        return resp.send( allCandidates );
+    router.get( '/candidate/:id', async (req, resp)=>{
+        try{
+            candidate = await Candidates.find({'_id':req.params.id});
+
+            return resp.send( candidate );
+
+        }
+        catch(err){
+            return resp.status(400).send({error:'The operation failed'});
+        }
+    
     })
 
-    router.delete('/', async (req, resp)=>{
-        allCandidates = await Candidates.find();
-        return resp.send( allCandidates );
+    router.put( '/candidate/:id', async (req, resp)=>{
+
+        try{
+            result = await Candidates.findByIdAndUpdate(req.params.id, req.body);
+            //todo: If there is no candidate with this id
+
+            candidateUpdated = await Candidates.findOne({'_id':req.params.id});
+
+            return resp.send(candidateUpdated);
+        }
+        catch(err){
+            console.log(err)
+            return resp.status(400).send({error:'The operation failed'})
+        }
+        
+    })
+
+    router.delete( '/candidate/:id', async (req, resp)=>{
+
+        try{
+            result = await Candidates.findByIdAndDelete(req.params.id);
+            //todo: If there is no candidate with this id
+
+            return resp.send(result);
+        }
+        catch(err){
+            return resp.status(400).send({error:'The operation failed'})
+        }
+
     })
 
     return router;
