0cc725bab22de72cc22d309a63a412df05d794e6
Alvaro Separovich <alvasepro@gmail.com>
2019-11-02 22:32:58 -0300

    Fix: required fields correction

--- src/app/models/job.js
+++ src/app/models/job.js
@@ -3,19 +3,19 @@ const db = require('../database');
 const JobSchema = new db.Schema({
     name:{
         type:String,
-        require:true,
+        required:[true,'A name is required'],
     },
     description:{
         type:String,
-        require:true,
+        required:[true,'A description is required'],
     },
     limitDate:{
         type:Date,
-        require:true,
+        required:[true,'A limitDate is required'],
     },
     vacancies:{
         type:Number,
-        require:true,
+        required:false,
     },
 });
 
