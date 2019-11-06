e1b2e63028ac01610c72a28bb36e7d0c7a5c5975
Alvaro Separovich <alvasepro@gmail.com>
2019-11-06 18:48:47 -0300

    Feat: Morgan Added

--- src/config/server-app.js
+++ src/config/server-app.js
@@ -2,9 +2,11 @@ const express = require('express');
 const app = express();
 const routes = require(`../app/routes`);
 const bodyParser = require('body-parser');
+const morgan = require('morgan');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
+app.use(morgan('tiny'));
 
 routes(app, express.Router);
 
