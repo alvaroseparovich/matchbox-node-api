9432807492173632617e2b0104dad84083ed21f8
Alvaro Separovich <alvasepro@gmail.com>
2019-11-06 00:13:24 -0300

    -Feat: Single Relation route created

--- src/app/routes/connections.js
+++ src/app/routes/connections.js
@@ -4,9 +4,12 @@ module.exports = (app,Router)=>{
 
     const router = Router();
     
-    router.put( '/add/:jobId', ( req,resp )=>{ Operator.createConnection( req,resp )} )
+    router.put( '/add/:jobId', ( req,resp )=>{ Operator.createConnection( req.body._id , req.params.jobId , resp )} )
 
     router.delete( '/remove/:jobId', ( req,resp )=>{ Operator.removeConnection( req,resp )} )
 
+    router.put( '/attatchjob/:jobId', ( req,resp )=>{ Operator.responseAddJobInCandidate( req.body._id , req.params.jobId , resp )} )
+
+    
     return router;
 }
